33e0347a8bea1dafbdfc35774f8eb64ef2cc6717
Hazim Bhat <103562501+Hammad-ctrl@users.noreply.github.com>
2024-11-07 19:52:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Preloader from './components/PreLoader';
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div >
-     <Navbar/>
-     <Routes >
-      <Route path='/' element={<Home/>}/>
-     </Routes>
+    <div>
+      {loading ? (
+        <Preloader /> 
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
